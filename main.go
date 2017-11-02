c905a15e8e7cde6fff3f8df2825511ce0cb0f399
Beck Chen <csybeck@gmail.com>
2017-11-02 00:04:59 -0700

    Shuffle

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -14,50 +16,77 @@ func main() {
 	var csvPath = flag.String("csv", "problems.csv", "Path to the input CSV file.")
 	var timeLimit time.Duration
 	flag.Int64Var((*int64)(&timeLimit), "time", 30, "Time limit to solve all quizzes, in seconds.")
+	var shuffle = flag.Bool("shuffle", false, "Shuffle the order of quizzes.")
 	flag.Parse()
 
 	content, err := ioutil.ReadFile(*csvPath)
 	check(err)
 	inputLines := strings.Split(string(content), "\n")
+	quizzes, answers := parseQuiz(&inputLines)
 
-	totalCount := 0
+	totalCount := len(*quizzes)
 	correctCount := 0
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Printf("Press Enter to start the quiz. You have %d seconds to finish the quiz.\n", timeLimit)
 	scanner.Scan()
 
-	go runQuiz(scanner, &totalCount, &correctCount, &inputLines)
-	timer := time.NewTimer(time.Second * timeLimit)
-	<-timer.C
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go runTimer(timeLimit, &wg)
+	go runQuiz(scanner, &correctCount, quizzes, answers, *shuffle, &wg)
+	wg.Wait() // Wait until either the timer goes off, or user answers all questions
 
-	fmt.Printf("You correctly answered %d out of %d questions\n", correctCount, totalCount)
+	fmt.Printf("\nYou correctly answered %d out of %d questions\n", correctCount, totalCount)
 }
 
-func runQuiz(scanner *bufio.Scanner, totalCount *int, correctCount *int, inputLines *[]string) {
-	for _, quiz := range *inputLines {
-		if len(quiz) > 0 {
-			(*totalCount)++
-			q, a := parseQuiz(quiz)
-			fmt.Printf("Question: %s\n", q)
-			fmt.Print("Answer: ")
-			scanner.Scan()
-			if a == scanner.Text() {
-				fmt.Println("Correct!")
-				(*correctCount)++
-			} else {
-				fmt.Println("Wrong...")
-			}
+func runTimer(timeLimit time.Duration, wg *sync.WaitGroup) {
+	timer := time.NewTimer(time.Second * timeLimit)
+	<-timer.C
+	(*wg).Done()
+}
+
+func parseQuiz(inputLines *[]string) (*[]string, *[]string) {
+	quizzes := make([]string, 0)
+	answers := make([]string, 0)
+	for _, line := range *inputLines {
+		if len(line) > 0 {
+			parts := strings.Split(line, ",")
+			quizzes = append(quizzes, parts[0])
+			answers = append(answers, parts[1])
 		}
 	}
+	return &quizzes, &answers
+}
+
+func runQuiz(scanner *bufio.Scanner, correctCount *int, quizzes *[]string, answers *[]string, shuffle bool, wg *sync.WaitGroup) {
+	rand.Seed(42)
+	order := rand.Perm(len(*quizzes))
+
+	for index, value := range order {
+		var q, a string
+		if shuffle {
+			q = (*quizzes)[value]
+			a = (*answers)[value]
+		} else {
+			q = (*quizzes)[index]
+			a = (*answers)[index]
+		}
+		fmt.Printf("Question: %s\n", q)
+		fmt.Print("Answer: ")
+		scanner.Scan()
+		if a == scanner.Text() {
+			fmt.Println("Correct!")
+			(*correctCount)++
+		} else {
+			fmt.Println("Wrong...")
+		}
+	}
+
+	(*wg).Done()
 }
 
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
-}
-
-func parseQuiz(quiz string) (string, string) {
-	parts := strings.Split(quiz, ",")
-	return parts[0], parts[1]
 }
